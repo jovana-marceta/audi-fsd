33a3238d3f24d0331f726644dca6b7c130180cda
jovana-marceta <jovanamarcetafrontenddeveloper@gmail.com>
2021-11-27 05:34:35 +0100

    Adding hero section and Swiper

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ import $ from 'jquery';
 window.jQuery = window.$ = $;
 
 
-
 import device from 'current-device';
 
 require('./app');
 require('./nav');
+require('./swiper');
 
 
 
---
+++ src/js/swiper.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".mySwiper", {
+	effect: "fade",
+	loop: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
+});
