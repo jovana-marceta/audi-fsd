eb475f14a764d5ec3a82ffa0dd4909422160d0ce
jovana-marceta <jovanamarcetafrontenddeveloper@gmail.com>
2021-12-01 00:27:15 +0100

    Adding dropdown

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,12 +1,22 @@
 const hamburger = document.querySelector(".js-hamburger");
 const navMenu = document.querySelector(".js-nav-menu");
+const navItems = document.querySelectorAll(".nav__item");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
 }))
+
+
+// Remove after element if list has dropdown menu
+navItems.forEach(item => {
+	item.classList.add("hasBorderBottom");
+	if (item.childNodes.length > 3) {
+		item.classList.remove("hasBorderBottom");
+	}
+})
